78943b71553afb5f10a46b85bdfd6586db65f81d
unknown <ganesh.srambikal@gmail.com>
2018-10-07 20:56:28 +0530

    part3 ADDED PUG FILE

--- app.js
+++ app.js
@@ -1,10 +1,44 @@
 const express = require('express');
-
+const path = require('path');
+//init app
 const app = express();
 
+//load view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// home route
 app.get('/', (req, res) => {
-    res.send('HEllo world');
+    let articles = [
+        {
+            id: 1,
+            title: 'Article one',
+            body: 'This Article one'
+        },
+        {
+            id: 2,
+            title: 'Article two',
+            body: 'This Article Two'
+        },
+        {
+            id: 3,
+            title: 'Article three',
+            body: 'This Article Three'
+        }
+    ];
+    res.render('index', {
+        title: 'hello',
+        articles: articles
+    });
 });
+
+//add route
+app.get('/articles/add', function (req, res) {
+    res.render('add_article', {
+        title: 'add article'
+    });
+});
+
 
 app.listen(3000, () => {
     console.log('server strted ..');
