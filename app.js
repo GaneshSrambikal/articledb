8d1f4de0e0bd1d66aa7c1aaa9ceb229223b742eb
unknown <ganesh.srambikal@gmail.com>
2018-10-07 21:54:01 +0530

    part4 model and mongoose setup

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/nodekb');
+let db = mongoose.connection;
+
+//check connection
+db.once('open', function () {
+    console.log('connected to MongoDB');
+});
+
+
+//check for db error
+db.on('error', function (err) {
+    console.log(err);
+});
+
 //init app
 const app = express();
+
+//Bring in model
+let Article = require('./models/article');
 
 //load view engine
 app.set('views', path.join(__dirname, 'views'));
@@ -9,27 +28,36 @@ app.set('view engine', 'pug');
 
 // home route
 app.get('/', (req, res) => {
-    let articles = [
-        {
-            id: 1,
-            title: 'Article one',
-            body: 'This Article one'
-        },
-        {
-            id: 2,
-            title: 'Article two',
-            body: 'This Article Two'
-        },
-        {
-            id: 3,
-            title: 'Article three',
-            body: 'This Article Three'
+    Article.find({}, function (err, articles) {
+        if (err) {
+            console.log(err);
         }
-    ];
-    res.render('index', {
-        title: 'hello',
-        articles: articles
+        else {
+            res.render('index', {
+                title: 'hello',
+                articles: articles
+            });
+        }
     });
+
+    // let articles = [
+    //     {
+    //         id: 1,
+    //         title: 'Article one',
+    //         body: 'This Article one'
+    //     },
+    //     {
+    //         id: 2,
+    //         title: 'Article two',
+    //         body: 'This Article Two'
+    //     },
+    //     {
+    //         id: 3,
+    //         title: 'Article three',
+    //         body: 'This Article Three'
+    //     }
+    // ];
+
 });
 
 //add route
