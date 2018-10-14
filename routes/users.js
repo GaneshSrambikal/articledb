6a5ca6fe29eb5ed7c78f77dde338bd0c203d51c2
unknown <ganesh.srambikal@gmail.com>
2018-10-14 21:18:28 +0530

    added register logic ,validation, login page,db successfully stores.

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
 //Bring in article model
 let User = require('../models/user');
@@ -8,6 +9,65 @@ let User = require('../models/user');
 //register form
 router.get('/register', (req, res) => {
     res.render('register');
+});
+
+//register process
+router.post('/register', (req, res) => {
+    const name = req.body.name;
+    const password = req.body.password;
+    const new_password = req.body.new_password;
+    const email = req.body.email;
+    const username = req.body.username;
+
+    req.checkBody('name', 'Name is required').notEmpty();
+    req.checkBody('email', 'Email is required').notEmpty();
+    req.checkBody('email', 'Email is not valid').isEmail();
+    req.checkBody('password', 'Password is required').notEmpty();
+    req.checkBody('new_password', 'Password do not match').equals(req.body.password);
+    req.checkBody('username', 'Username is required').notEmpty();
+
+    let errors = req.validationErrors();
+
+    if (errors) {
+        res.render('register', {
+            errors: errors
+        });
+    } else {
+        let newUser = new User({
+            name: name,
+            email: email,
+            username: username,
+            password: password
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    newUser.password = hash;
+                    newUser.save((err) => {
+                        if (err) {
+                            console.log(err);
+                            return;
+                        }
+                        else {
+                            req.flash('success', 'You are registered and can loginIn');
+                            console.log('You are registered and can loginIn');
+                            res.redirect('/users/login');
+                        }
+
+                    });
+                }
+            });
+        });
+    }
+
+});
+
+router.get('/login', (req, res) => {
+    res.render('login');
 });
 
 module.exports = router;
